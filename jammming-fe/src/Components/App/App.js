beb3941a625769961f8e996a30a64c52c0b99f18
misathemeb <melissabridi@gmail.com>
2022-03-14 16:08:03 -0400

    added 3 components

---
+++ jammming-fe/src/Components/App/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+
+function App() {
+  return (
+    <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+   { /* Add a SearchBar component */ }
+    <SearchBar />
+    <div className="App-playlist">
+    { /* Add a SearchResults component 
+      Add a Playlist component */ }
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default App;

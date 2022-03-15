15afcaf66e60dcc6b74f754bad8bf8aa84716111
misathemeb <melissabridi@gmail.com>
2022-03-15 13:58:49 -0400

    added constructor and vars to app.js

--- jammming-fe/src/Components/App/App.js
+++ jammming-fe/src/Components/App/App.js
@@ -1,21 +1,42 @@
 
+import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import PlayList from '..PlayList';
 
-function App() {
-  return (
-    <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-   { /* Add a SearchBar component */ }
-    <SearchBar />
-    <div className="App-playlist">
-    { /* Add a SearchResults component 
-      Add a Playlist component */ }
+
+class App extends React.Component {
+  constructor(props){
+  super(props);
+  this.state = {
+    SearchResults: [],
+    playlistName: 'My playlist',
+    playlistTracks: []
+  };
+
+this.addTracks = this.addTracks.bind(this);
+this.removeTrack = this.removeTrack.bind(this);
+this.updatePlaylistName = this.updatePlaylistName.bind(this);
+this.savePlaylist = this.savePlaylist.bind(this);
+this.search = this.search.bind(this);
+}
+
+  render() {
+      return (
+        <div>
+          <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+          <SearchBar />
+
+            <div className="App-playlist">
+                <SearchResults />
+                <PlayList />
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-  );
+    )
+}
 }
 
 export default App;

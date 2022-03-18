25a276127ebf99e54edd7d2429dfc949115a48b6
misathemeb <melissabridi@gmail.com>
2022-03-18 15:06:58 -0400

    added header constants

--- jammming-fe/src/util/Spotify.js
+++ jammming-fe/src/util/Spotify.js
@@ -1,6 +1,8 @@
 
 // need a clientID and redirectURI
-
+const clientID = '583c94ecbb034584a24901479f3c6194';
+const redirectURI = "http://localhost:3000/" //or surge deployment url?
+const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientID}&redirect_uri=${redirectURI}`;
 
 let token = {};
 class Spotify {
@@ -8,17 +10,17 @@ class Spotify {
     getAccessToken(){
         if(token) {
             return token;
+        } 
+        //check to see if tokens match
+        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const expiredToken = window.location.href.match(/expires_in=([^&]*)/);
+        if (accessToken === expiredToken){
+            window.history.pushState('Access Token has expired', null); 
+            
         } else {
-            //check to see if tokens match
-            const tokenMatch = window.location.href.match(/access_token=([^&]*)/)
-            const expiredMatch = window.location.href.match(/expires_in=([^&]*)/)
+            window.location = spotifyUrl;
         }
-
     }
-
-
-
-
 
 };
 
